10f90433294978e15ad1704e583f2d63a1943ed5
Hemachandra Ghanta <hemachandra.ghanta@Yash.com>
2021-03-10 20:07:51 -0600

    initial commit

---
+++ TextReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace TextReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Directory.GetCurrentDirectory() + @"\Important.txt";
+
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = "File Does not exist. Please check..." + Environment.NewLine;
+                File.WriteAllText(path, createText, Encoding.UTF8);
+            }
+
+            // Open the file to read from.
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+        }
+    }
+}
